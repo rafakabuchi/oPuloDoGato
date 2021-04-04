36dd350eacf812e498b74e186aaae2c201225ce8
Rafa Kabuchi <rkabuchi@gmail.com>
2021-04-04 16:43:27 -0300

    criada pagina encontrei

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,19 @@ const showData = (result)=>{
             document.querySelector("#"+campo).value = result[campo]
         }
     }
+    const rua = result.logradouro
+
+    const mapa = document.querySelector('#map')
+    mapa.innerHTML = `<iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCPhQT9tVX9h1JHGb8KyfLZ9L40k6V57qQ&q=${rua},Brasil" frameborder="0"></iframe>`
+
+    function change(){
+        const search = document.querySelector('#search')
+        search.disabled = true
+        research.disabled = false
+        cep.readOnly = true
+        errorMessage.innerHTML = ''
+    }
+    change();
 }
 
 cep.addEventListener("blur", (e) => {
@@ -23,3 +36,17 @@ cep.addEventListener("blur", (e) => {
         })
         .catch(e => console.log('Deu erro: ' + e.message))
 })
+
+function phoneMaskBrazil() {
+    var key = window.event.key;
+    var element = window.event.target;
+    var isAllowed = /\d|Backspace|Tab/;
+    if(!isAllowed.test(key)) window.event.preventDefault();
+
+    var inputValue = element.value;
+    inputValue = inputValue.replace(/\D/g,'');
+    inputValue = inputValue.replace(/(^\d{2})(\d)/,'($1) $2');
+    inputValue = inputValue.replace(/(\d{4,5})(\d{4}$)/,'$1-$2');
+    
+    element.value = inputValue;
+  }
