f5bf252e1a98fc73b572fc46ae52968bbb14b1dc
Kevin <kevinsadao@hotmail.com>
2021-06-05 17:35:05 -0300

    preferencia de tema salvo

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,16 +1,29 @@
 const btn = document.getElementById('switch-theme');
-localStorage.setItem('theme','light');
-btn.addEventListener('click',(e)=>{
-     let theme = localStorage.getItem('theme');
-     console.log(theme);
-     if(theme == 'light' || theme ==''){
-          document.documentElement.setAttribute('data-theme','dark');
-          document.getElementById("logo").src="./assets/favicon/favicon-32x32 dark-mode.png";
-          localStorage.setItem('theme','dark');
+let theme = localStorage.getItem('theme');
+console.log(theme);
+
+document.addEventListener("DOMContentLoaded", (e) => {
+     if (theme == 'dark') {
+          document.documentElement.setAttribute('data-theme', 'dark');
+          document.getElementById("logo").src = "./assets/favicon/favicon-32x32 dark-mode.png";
+          localStorage.setItem('theme', 'dark');
      }
      else {
-          document.documentElement.removeAttribute('data-theme');          
-          document.getElementById("logo").src="./assets/favicon/favicon-32x32.png";
-          localStorage.setItem('theme','light');
+          document.documentElement.removeAttribute('data-theme');
+          document.getElementById("logo").src = "./assets/favicon/favicon-32x32.png";
+          localStorage.setItem('theme', 'light');
+     }
+});
+
+btn.addEventListener('click', (e) => {     
+     if (theme == 'dark') {
+          document.documentElement.removeAttribute('data-theme');
+          document.getElementById("logo").src = "./assets/favicon/favicon-32x32.png";
+          localStorage.setItem('theme', 'light');
+     }
+     else {
+          document.documentElement.setAttribute('data-theme', 'dark');
+          document.getElementById("logo").src = "./assets/favicon/favicon-32x32 dark-mode.png";
+          localStorage.setItem('theme', 'dark');
      }
 })
