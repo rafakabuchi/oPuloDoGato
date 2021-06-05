0fbc073487f0b6bd53d1a71d8719b4fd2f66d701
Kevin <kevinsadao@hotmail.com>
2021-06-05 15:44:47 -0300

    ajuste js dark-mode e cadastroPessoal

---
+++ assets/js/cadastroPessoal.js
@@ -0,0 +1,24 @@
+// function validarForm(){
+//   var nome = document.getElementById("nome");
+//   var nascimento = document.getElementById("nascimento");
+//   var cpf = document.getElementById("nome");
+//   var nome = document.getElementById("nome");
+//   var nome = document.getElementById("nome");
+//   var nome = document.getElementById("nome");
+//   var nome = document.getElementById("nome");
+
+//   if (nome.value == ""){
+//     alert("Preencha o nome");  
+//   } else if (nascimento.value == ""){
+//     alert("Preencha a data de nascimento");  
+//   } else if (cpf.value == ""){
+//     alert("Preencha o CPF");  
+//   } else if (nascimento.value == ""){
+//     alert("Preencha a data de nascimento");  
+//   } else if (nascimento.value == ""){
+//     alert("Preencha a data de nascimento");  
+//   } else if (nascimento.value == ""){
+//     alert("Preencha a data de nascimento");  
+//   }
+//   alert("js " + nome.value);
+// }
